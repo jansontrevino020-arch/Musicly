20ef82b31859731f3f1ed7f4a5ab9ac17ddbae1b
jansontrevino020-arch <jansontrevino020@gmail.com>
2026-04-07 07:02:31 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,21 @@
-const CACHE = "musicly-cache-v8";
+const CACHE = "musicly-cache-v9";
 
 const FILES = [
+  "/Musicly",
   "/Musicly/",
   "/Musicly/index.html",
   "/Musicly/app.js",
   "/Musicly/jszip.min.js",
-  "/Musicly/manifest.webmanifest"
+  "/Musicly/manifest.webmanifest",
+  "/Musicly/icon-180.png",
+  "/Musicly/icon-512.png"
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
@@ -18,17 +24,16 @@ self.addEventListener("activate", event => {
       Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
     )
   );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request, { ignoreSearch: true }).then(response => {
       if (response) return response;
 
       return fetch(event.request).catch(() => {
-        if (event.request.mode === "navigate") {
-            return caches.match("/Musicly/index.html");
-        }
+        return caches.match("/Musicly/index.html");
       });
     })
   );
